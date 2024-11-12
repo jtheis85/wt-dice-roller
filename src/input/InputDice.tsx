bfa4daf0e9b1c107f5c206a7fc3768f8994a54af
Joe Theis <jatheis2@gmail.com>
2024-11-12 13:48:55 -0800

    Add dice button floating over input

--- src/input/InputDice.tsx
+++ src/input/InputDice.tsx
@@ -1,11 +1,19 @@
-import "./input.css";
-
 import React from "react";
+import "./button.css";
+import "./input.css";
+import addDice from "../assets/add-dice.svg";
 
 interface Props {}
 
 const InputDice: React.FC<Props> = ({}) => {
-  return <input className="input-dice" placeholder="/r 2d20kh1+1d4+5" />;
+  return (
+    <div>
+      <button className="button-add-dice minimal">
+        <img src={addDice} />
+      </button>
+      <input className="input-dice" placeholder="/r 2d20kh1+1d4+5" />
+    </div>
+  );
 };
 
 export default InputDice;
